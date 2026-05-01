70df2b741f0b2853e75cb8df69e9819a0402f051
William Rockwood <wrockwood@tadl.org>
2026-04-30 21:41:19 -0400

    Add system test harness

---
+++ test/system/home_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+class HomeTest < ApplicationSystemTestCase
+  test "visiting the photo stream" do
+    visit root_path
+
+    assert_text "wjr photos"
+    assert_button "Sign in"
+    assert_text "ARCHIVE RULE"
+  end
+end
